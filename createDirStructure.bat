39e4754767610f724ba65c5f7e29d6563bb190ec
Francisco Baptista <chico.baptista@gmail.com>
2018-04-20 16:41:44 -0300

    added createDirStructure
    simple script to create the file structure for translation projects

---
+++ createDirStructure.bat
@@ -0,0 +1,12 @@
+rem creates the following file structure for new translations:
+rem -- originals
+rem -- working --
+rem              \-- 00-preview
+rem              \-- 01-matecat_input
+rem              \-- 02-matecat_output
+rem              \-- 03-transpdf_output
+rem              \-- 04-infix_output
+rem              \-- 05-certificates
+rem -- deliverables
+md originals deliverables working
+md working\00-preview working\01-matecat_input working\02-matecat_output working\03-transpdf_output working\04-infix_output working\05-certificates
